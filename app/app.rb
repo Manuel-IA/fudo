8614e2d74d0f5b93e7fef3329fb43eb32f1465ed
Manuel Esteban Palacio <56773748+Manuel-IA@users.noreply.github.com>
2024-11-18 15:36:34 -0500

    Added auth endpoint

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,9 @@
 require 'json'
+require 'jwt'
 
 class App
+  SECRET_KEY = 'my$ecretK3y'
+
   def call(env)
     req = Rack::Request.new(env)
     res = Rack::Response.new
@@ -10,6 +13,8 @@ class App
       res['Content-Type'] = 'application/json'
       res.write({ message: "Ruby API" }.to_json)
       res.status = 200
+    when '/auth'
+      authenticate(req, res)
     else
       res['Content-Type'] = 'application/json'
       res.write({ error: "Page Not Found" }.to_json)
@@ -17,5 +22,42 @@ class App
     end
 
     res.finish
+  end
+
+  private
+
+  def authenticate(req, res)
+    if req.post?
+      username = req.params['username']
+      password = req.params['password']
+
+      if username == 'admin' && password == 'password'
+        token = generate_jwt(username)
+        res['Content-Type'] = 'application/json'
+        res.write({ message: "Authentication Successful", token: token }.to_json)
+        res.status = 200
+      else
+        res['Content-Type'] = 'application/json'
+        res.write({ error: "Unauthorized" }.to_json)
+        res.status = 401
+      end
+    else
+      res['Content-Type'] = 'application/json'
+      res.write({ error: "Method Not Allowed" }.to_json)
+      res.status = 405
+    end
+  end
+
+  def generate_jwt(username)
+    payload = {
+      username: username,
+      exp: Time.now.to_i + 3600
+    }
+    JWT.encode(payload, SECRET_KEY, 'HS256')
+  end
+
+  def decode_jwt(token)
+    decoded_token = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
+    decoded_token.first
   end
 end
